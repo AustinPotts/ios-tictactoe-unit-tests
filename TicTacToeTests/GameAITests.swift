b42c29828e643d0782bee407d7214ff97897dc30
AustinPotts <pottsevolvements@gmail.com>
2019-11-12 16:07:57 -0500

    Added GameAI Unit Test Case Class

---
+++ TicTacToeTests/GameAITests.swift
@@ -0,0 +1,17 @@
+//
+//  GameAITests.swift
+//  TicTacToeTests
+//
+//  Created by Austin Potts on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameAITests: XCTestCase {
+
+ 
+
+  
+
+}
